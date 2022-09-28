919ad60ef266ed2a9e731024cd1b3fcef61c19a7
Akilesh Tangella <akileshtangella@macbook-air.myfiosgateway.com>
2022-09-28 13:25:21 -0400

    start on fs-dkr state machine

---
+++ src/refresh/rounds.rs
@@ -0,0 +1,25 @@
+use crate::keygen::rounds::*;
+
+pub struct Round0 {
+    local_key: Option<LocalKey<Secp256k1>>
+}
+
+impl Round0 {
+
+}
+
+pub struct Round1 {
+    local_key: LocalKey<Secp256k1>,
+}
+
+impl Round1 {
+
+}
+
+pub struct Round2 {
+    local_key: LocalKey<Secp256k1>,
+}
+
+impl Round2 {
+    
+}
