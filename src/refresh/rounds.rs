2a05e88dfe335cb106be867d9f606b3037596ac2
Akilesh Tangella <akileshtangella@macbook-air.myfiosgateway.com>
2022-09-28 13:50:35 -0400

    round0 existing party case

--- src/refresh/rounds.rs
+++ src/refresh/rounds.rs
@@ -1,15 +1,44 @@
 use crate::keygen::rounds::*;
+use fs_dkr::{add_party_message::*, refresh_message::*};
+use round_based::Msg;
 
 pub struct Round0 {
-    local_key: Option<LocalKey<Secp256k1>>
+	local_key_option: Option<LocalKey<Secp256k1>>
 }
 
 impl Round0 {
+	pub fn proceed<O>(self, mut output: O) -> Result<Round1>
+    where
+        O: Push<Msg<>>,
+    {
+        // Match on local_key
+		match local_key_option {
+			Some(local_key) => {
+				// Push JoinMessage to output
+				output.push(Msg {
+					sender: local_key.i,
+					receiver: None,
+					body: None,
+				});
+				// Send local key to round 1
+				Ok(Round1 {
+					local_key: local_key,
+				})
+			},
+			None => {
+				// Push JoinMessage to output
+				// Generate new local key
+			}
+		}
 
+    }
+    pub fn is_expensive(&self) -> bool {
+		true
+    }  
 }
 
 pub struct Round1 {
-    local_key: LocalKey<Secp256k1>,
+	local_key: LocalKey<Secp256k1>,
 }
 
 impl Round1 {
@@ -17,9 +46,9 @@ impl Round1 {
 }
 
 pub struct Round2 {
-    local_key: LocalKey<Secp256k1>,
+	local_key: LocalKey<Secp256k1>,
 }
 
 impl Round2 {
-    
+
 }
