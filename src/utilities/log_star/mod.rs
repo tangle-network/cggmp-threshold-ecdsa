4f7004d40df8f19835300db99e37dffeda8a61eb
Akilesh Tangella <saiakilesh@gmail.com>
2022-10-28 14:04:24 -0400

    log star works

--- src/utilities/log_star/mod.rs
+++ src/utilities/log_star/mod.rs
@@ -34,18 +34,18 @@ use curv::{
 };
 use paillier::{EncryptWithChosenRandomness, EncryptionKey, Paillier, Randomness, RawPlaintext};
 use serde::{Deserialize, Serialize};
-use zk_paillier::zkproofs::IncorrectProof;
 use std::marker::PhantomData;
+use zk_paillier::zkproofs::IncorrectProof;
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct KnowledgeOfExponentPaillierEncyptionStatement<E: Curve, H: Digest + Clone> {
 	N0: BigInt,
-    NN0: BigInt,
+	NN0: BigInt,
 	C: BigInt,
-	X: BigInt,
-    N_hat: BigInt,
-    s: BigInt,
-    t: BigInt,
+	X: Point<E>,
+	N_hat: BigInt,
+	s: BigInt,
+	t: BigInt,
 	phantom: PhantomData<(E, H)>,
 }
 
@@ -58,13 +58,13 @@ pub struct KnowledgeOfExponentPaillierEncyptionWitness<E: Curve, H: Digest + Clo
 impl<E: Curve, H: Digest + Clone> KnowledgeOfExponentPaillierEncyptionStatement<E, H> {
 	#[allow(clippy::too_many_arguments)]
 	pub fn generate(
-        rho: BigInt,
+		rho: BigInt,
 		s: BigInt,
 		t: BigInt,
 		N_hat: BigInt,
 		paillier_key: EncryptionKey,
 	) -> (Self, KnowledgeOfExponentPaillierEncyptionWitness<E, H>) {
-        // Set up exponents
+		// Set up exponents
 		let l_exp = BigInt::pow(&BigInt::from(2), L as u32);
 		let lprime_exp = BigInt::pow(&BigInt::from(2), L_PRIME as u32);
 		// Set up moduli
@@ -72,19 +72,14 @@ impl<E: Curve, H: Digest + Clone> KnowledgeOfExponentPaillierEncyptionStatement<
 		let NN0 = paillier_key.clone().nn;
 		let x = BigInt::sample_range(&BigInt::from(-1).mul(&l_exp), &l_exp);
 		let X = Point::<E>::generator().as_point() * Scalar::from(&x);
-        let C = Paillier::encrypt_with_chosen_randomness(&EncryptionKey { n: N0.clone(), nn: NN0.clone() }, RawPlaintext::from(&x),
-        &Randomness::from(&rho),);
+		let C: BigInt = Paillier::encrypt_with_chosen_randomness(
+			&EncryptionKey { n: N0.clone(), nn: NN0.clone() },
+			RawPlaintext::from(&x),
+			&Randomness::from(&rho),
+		)
+		.into();
 		(
-			Self {
-                N0,
-                NN0,
-                C,
-                X,
-                N_hat,
-                s,
-                t,
-				phantom: PhantomData,
-			},
+			Self { N0, NN0, C, X, N_hat, s, t, phantom: PhantomData },
 			KnowledgeOfExponentPaillierEncyptionWitness { x, rho, phantom: PhantomData },
 		)
 	}
@@ -92,13 +87,13 @@ impl<E: Curve, H: Digest + Clone> KnowledgeOfExponentPaillierEncyptionStatement<
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct KnowledgeOfExponentPaillierEncyptionProof<E: Curve, H: Digest + Clone> {
-    S: BigInt,
-    A: BigInt,
-    Y: Point<E>,
-    D: BigInt,
-    z_1: BigInt,
-    z_2: BigInt,
-    z_3: BigInt,
+	S: BigInt,
+	A: BigInt,
+	Y: Point<E>,
+	D: BigInt,
+	z_1: BigInt,
+	z_2: BigInt,
+	z_3: BigInt,
 	phantom: PhantomData<(E, H)>,
 }
 
@@ -108,12 +103,12 @@ impl<E: Curve, H: Digest + Clone> KnowledgeOfExponentPaillierEncyptionProof<E, H
 		witness: &KnowledgeOfExponentPaillierEncyptionWitness<E, H>,
 		statement: &KnowledgeOfExponentPaillierEncyptionStatement<E, H>,
 	) -> KnowledgeOfExponentPaillierEncyptionProof<E, H> {
-        // Step 1: Sample alpha between -2^{l+ε} and 2^{l+ε}
-        let alpha_upper = BigInt::pow(&BigInt::from(2), crate::utilities::L_PLUS_EPSILON as u32);
-        let alpha_lower = BigInt::from(-1).mul(&alpha_upper);
-        let alpha = BigInt::sample_range(&alpha_lower, &alpha_upper);
+		// Step 1: Sample alpha between -2^{l+ε} and 2^{l+ε}
+		let alpha_upper = BigInt::pow(&BigInt::from(2), crate::utilities::L_PLUS_EPSILON as u32);
+		let alpha_lower = BigInt::from(-1).mul(&alpha_upper);
+		let alpha = BigInt::sample_range(&alpha_lower, &alpha_upper);
 
-        // Step 2: mu, r, gamma
+		// Step 2: mu, r, gamma
 		// Sample mu between -2^L * N_hat and 2^L * N_hat
 		let mu_upper = BigInt::mul(
 			&statement.N_hat,
@@ -132,31 +127,38 @@ impl<E: Curve, H: Digest + Clone> KnowledgeOfExponentPaillierEncyptionProof<E, H
 		// Sample r from Z*_{N_0}
 		let r = sample_relatively_prime_integer(&statement.N0.clone());
 
-        // S = s^x t^mu mod N_hat
+		// S = s^x t^mu mod N_hat
 		let S = BigInt::mod_mul(
 			&mod_pow_with_negative(&statement.s, &witness.x, &statement.N_hat),
 			&mod_pow_with_negative(&statement.t, &mu, &statement.N_hat),
 			&statement.N_hat,
 		);
 
-        // A = (1+N_0)^{alpha}r^{N_0} mod N_0^2
-        let N0_squared = BigInt::mul(&statement.N_0, &statement.N_0);
-        let A: BigInt = Paillier::encrypt_with_chosen_randomness(&EncryptionKey { n: statement.N0.clone(), nn: statement.NN0.clone() }, RawPlaintext::from(&alpha),
-        &Randomness::from(&r),);
+		// A = (1+N_0)^{alpha}r^{N_0} mod N_0^2
+		let A: BigInt = Paillier::encrypt_with_chosen_randomness(
+			&EncryptionKey { n: statement.N0.clone(), nn: statement.NN0.clone() },
+			RawPlaintext::from(&alpha),
+			&Randomness::from(&r),
+		)
+		.into();
 
-        // Y = g^alpha
-        let Y = Point::<E>::generator().as_point() * Scalar::from_bigint(&alpha);
-
-        // D = s^alpha t^gamma mod N_hat
-        let D = BigInt::mod_mul(
+		// Y = g^alpha
+		let Y = Point::<E>::generator().as_point() * Scalar::from_bigint(&alpha);
+		// D = s^alpha t^gamma mod N_hat
+		let D = BigInt::mod_mul(
 			&mod_pow_with_negative(&statement.s, &alpha, &statement.N_hat),
 			&mod_pow_with_negative(&statement.t, &gamma, &statement.N_hat),
 			&statement.N_hat,
 		);
 
-        let e = H::new().chain_bigint(&S).chain_bigint(&A).chain_bigint(&Y).chain_bigint(&D).result_bigint();
+		let e = H::new()
+			.chain_bigint(&S)
+			.chain_bigint(&A)
+			.chain_point(&Y)
+			.chain_bigint(&D)
+			.result_bigint();
 
-        // Step 5: Compute z_1, z_2, z_3
+		// Step 5: Compute z_1, z_2, z_3
 		// z_1 = alpha + ex
 		let z_1 = BigInt::add(&alpha, &BigInt::mul(&e, &witness.x));
 		// z_2 = r * rho^e mod N_0
@@ -168,72 +170,70 @@ impl<E: Curve, H: Digest + Clone> KnowledgeOfExponentPaillierEncyptionProof<E, H
 		// z_3 = gamma + e*mu
 		let z_3 = BigInt::add(&gamma, &BigInt::mul(&e, &mu));
 
-        Self {
-            S,
-            A,
-            Y,
-            D,
-            z_1,
-            z_2,
-            z_3,
-            phantom: PhantomData,
-        }
+		Self { S, A, Y, D, z_1, z_2, z_3, phantom: PhantomData }
 	}
 
 	pub fn verify(
 		proof: &KnowledgeOfExponentPaillierEncyptionProof<E, H>,
 		statement: &KnowledgeOfExponentPaillierEncyptionStatement<E, H>,
-	) -> Result<(), Error> {
-        let e = H::new().chain_bigint(&proof.S).chain_bigint(&proof.A).chain_bigint(&proof.Y).chain_bigint(&proof.D).result_bigint();
+	) -> Result<(), IncorrectProof> {
+		let e = H::new()
+			.chain_bigint(&proof.S)
+			.chain_bigint(&proof.A)
+			.chain_point(&proof.Y)
+			.chain_bigint(&proof.D)
+			.result_bigint();
 
 		// left_1 = (1+N_0)^{z_1}z_2^{N_0} mod N_0^2
 		let left_1: BigInt = Paillier::encrypt_with_chosen_randomness(
-			&EncryptionKey { n: statement.N0.clone(), nn: statement.NN0 },
+			&EncryptionKey { n: statement.N0.clone(), nn: statement.NN0.clone() },
 			RawPlaintext::from(&proof.z_1),
 			&Randomness::from(&proof.z_2),
 		)
 		.into();
 
-        // right_1 = A * C^e
-        let right_1 =
-        BigInt::mod_mul(&proof.A, &mod_pow_with_negative(&statement.C, &e, &statement.NN0), &statement.NN0);
+		// right_1 = A * C^e
+		let right_1 = BigInt::mod_mul(
+			&proof.A,
+			&mod_pow_with_negative(&statement.C, &e, &statement.NN0),
+			&statement.NN0,
+		);
 
-        // left_2 = g^z_1
-        let left_2 = Point::<E>::generator().as_point() * Scalar::from_bigint(&proof.z_1);
-        // right_2 = Y * X^e
-        let right_2 = proof.Y.clone() + (statement.X.clone() * Scalar::from_bigint(&e));
-
+		// left_2 = g^z_1
+		let left_2 = Point::<E>::generator().as_point() * Scalar::from_bigint(&proof.z_1);
+		// right_2 = Y * X^e
+		let right_2 = proof.Y.clone() + (statement.X.clone() * Scalar::from_bigint(&e));
 
 		// left_3 = s^z_1 t^z_3 mod N_hat
-        let left_3 = BigInt::mod_mul(
+		let left_3 = BigInt::mod_mul(
 			&mod_pow_with_negative(&statement.s, &proof.z_1, &statement.N_hat),
 			&mod_pow_with_negative(&statement.t, &proof.z_3, &statement.N_hat),
 			&statement.N_hat,
 		);
 
-        // right_3 = D * S^e mod N_hat
-        let right_3 = BigInt::mod_mul(
+		// right_3 = D * S^e mod N_hat
+		let right_3 = BigInt::mod_mul(
 			&proof.D,
 			&mod_pow_with_negative(&proof.S, &e, &statement.N_hat),
 			&statement.N_hat,
 		);
 
-        if left_1.mod_floor(&statement.NN0) != right_1 || left_2 != right_2 || left_3 != right_3 {
-            return Err(IncorrectProof)
-        }
+		if left_1.mod_floor(&statement.NN0) != right_1 || left_2 != right_2 || left_3 != right_3 {
+			return Err(IncorrectProof)
+		}
 
-        // Range Check -2^{L + eps} <= z_1 <= 2^{L+eps}
+		// Range Check -2^{L + eps} <= z_1 <= 2^{L+eps}
 		let lower_bound_check: bool = proof.z_1 >=
-        BigInt::from(-1)
-            .mul(&BigInt::pow(&BigInt::from(2), crate::utilities::L_PLUS_EPSILON as u32));
+			BigInt::from(-1)
+				.mul(&BigInt::pow(&BigInt::from(2), crate::utilities::L_PLUS_EPSILON as u32));
 
-        let upper_bound_check =
-            proof.z_1 <= BigInt::pow(&BigInt::from(2), crate::utilities::L_PLUS_EPSILON as u32);
+		let upper_bound_check =
+			proof.z_1 <= BigInt::pow(&BigInt::from(2), crate::utilities::L_PLUS_EPSILON as u32);
 
-        if !(lower_bound_check && upper_bound_check) {
-            return Err(IncorrectProof)
-        }
-        Ok(())
+		if !(lower_bound_check && upper_bound_check) {
+			return Err(IncorrectProof)
+		}
+		Ok(())
 	}
 }
 
@@ -256,8 +256,8 @@ mod tests {
 		let (statement, witness) =
 			KnowledgeOfExponentPaillierEncyptionStatement::<Secp256k1, Sha256>::generate(
 				rho,
-				ring_pedersen_statement.s,
-				ring_pedersen_statement.t,
+				ring_pedersen_statement.S,
+				ring_pedersen_statement.T,
 				ring_pedersen_statement.N,
 				paillier_key,
 			);
