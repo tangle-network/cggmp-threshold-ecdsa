eb1b31d975d56bc2767400bcd4f9d5fc02c8e17b
Akilesh Tangella <saiakilesh@gmail.com>
2022-11-09 20:28:40 -0500

    add licenses

--- src/sign/state_machine.rs
+++ src/sign/state_machine.rs
@@ -1,3 +1,18 @@
+/*
+	CGGMP Threshold ECDSA
+
+	Copyright 2022 by Webb Technologies.
+
+	This file is part of cggmp library
+	(https://github.com/webb-tools/cggmp-threshold-ecdsa)
+
+	cggmp-threshold-ecdsa is free software: you can redistribute
+	it and/or modify it under the terms of the GNU General Public
+	License as published by the Free Software Foundation, either
+	version 3 of the License, or (at your option) any later version.
+
+	@license GPL-3.0+ <https://github.com/webb-tools/cggmp/blob/main/LICENSE>
+*/
 use crate::presign::{PresigningOutput, PresigningTranscript, SSID};
 
 use super::{
